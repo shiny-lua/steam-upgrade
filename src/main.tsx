4605dc9e4e03ad72f63c5c2d6f6baef8a6ca1dfe
shiny-lua <sol.trx.bf@gmail.com>
2025-01-17 14:24:28 +0000

    remove: carusel

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import './index.css';
 // import 'jsvectormap/dist/css/jsvectormap.css';
 import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from "@material-tailwind/react";
-import 'react-multi-carousel/lib/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
