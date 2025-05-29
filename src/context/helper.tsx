b73f8fc953b3e97b3405c98626f794b0fb138b0f
shiny-lua <sol.trx.bf@gmail.com>
2025-05-29 08:28:13 -0700

    fix: remove unneccessary function

--- src/context/helper.tsx
+++ src/context/helper.tsx
@@ -235,10 +235,10 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-export function hexToRgba(hex: string, alpha: number) {
-    const r = parseInt(hex.slice(1, 3), 16);
-    const g = parseInt(hex.slice(3, 5), 16);
-    const b = parseInt(hex.slice(5, 7), 16);
+// export function hexToRgba(hex: string, alpha: number) {
+//     const r = parseInt(hex.slice(1, 3), 16);
+//     const g = parseInt(hex.slice(3, 5), 16);
+//     const b = parseInt(hex.slice(5, 7), 16);
 
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
-}
+//     return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+// }
